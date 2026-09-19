1a9b1c5b66f2b635fe339d87c3408f0d7ba69b04
assessoriaprimeoficial-tech <assessoriaprime.oficial@gmail.com>
2026-09-18 21:01:07 -0300

    chore: remove temporary offer guide helper

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { OfferGuideFix } from "@/components/upsell/OfferGuideFix";
 import { TuGemeloDigitalPage } from "@/components/upsell/TuGemeloDigitalPage";
-
-function Page() {
-  return (
-    <>
-      <TuGemeloDigitalPage />
-      <OfferGuideFix />
-    </>
-  );
-}
 
 export const Route = createFileRoute("/")({
   head: () => ({
@@ -36,5 +26,5 @@ export const Route = createFileRoute("/")({
       { name: "twitter:card", content: "summary_large_image" },
     ],
   }),
-  component: Page,
+  component: TuGemeloDigitalPage,
 });
